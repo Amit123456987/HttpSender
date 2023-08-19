2eadf0d8b5563fd42d7e8b0d7f88a18bdb05b960
Amit123456987 <43910117+Amit123456987@users.noreply.github.com>
2023-08-19 10:52:29 -0400

    Update main.dart

--- main.dart
+++ main.dart
@@ -32,47 +32,50 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  var data = "";
   var url = Uri.parse('http://192.168.1.6:8000');
   final TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _controller,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
-              ),
-            ),
-            TextButton(
-              onPressed: (){
-                var value = _controller.text;
-                print(value);
-
-                http.post(
-                  url,
-                  headers: <String, String>{
-                    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                  },
-                  body: {
-                    'text': value
-                  }
-                );
-              },
-              child: Text("Send")
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
         ),
-      )
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                controller: _controller,
+                onChanged: (value){
+                    data = value;
+                },
+                onTap: () => _controller.selection = TextSelection(
+                    baseOffset: 0, extentOffset: _controller.value.text.length),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+              TextButton( onFocusChange: (True) {
+                    _controller.selection;
+                  },
+                  onPressed: () {
+                    var value = data;
+                    print("value: "+value);
+
+                    http.post(url, headers: <String, String>{
+                      'Content-Type':
+                          'application/x-www-form-urlencoded; charset=UTF-8',
+                    }, body: {
+                      'text': value
+                    });
+                  },
+                  child: Text("Send")),
+            ],
+          ),
+        ));
   }
 }
